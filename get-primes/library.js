cfea50efda9c13aed2c5b0b40752ba6023a1fa7d
seunkoko <seunowonikoko@gmail.com>
2017-01-09 00:53:11 +0100

    The boilerplate for the getPrimes function

---
+++ get-primes/library.js
@@ -0,0 +1,14 @@
+
+    /*
+    * Get Primes --> gets the prime numbers from 0 to any number used to call the function
+    */
+
+    'use strict'
+  
+    module.exports = {
+
+		getPrimes: function (num) {
+            
+		}
+
+	}
---
+++ get-primes/tests/test.js
@@ -0,0 +1,9 @@
+
+    'use strict';
+
+    var getPrimes = require('../library.js').getPrimes;
+
+    describe("Get Prime Tests ", function() {
+        
+
+    });
