1d37859029e61d2d63b104118b999baf986cf61f
seunkoko <seunowonikoko@gmail.com>
2017-01-08 23:45:23 +0100

    Function for data-types-lab written and tested

--- data-types-lab/library.js
+++ data-types-lab/library.js
@@ -1,13 +1,39 @@
 	/*
-  * Data Types Lab --> http://labs.andela.com/labs/data-types-lab
-  */
+    * Data Types Lab --> http://labs.andela.com/labs/data-types-lab
+    */
 
-  'use strict'
-
-	module.exports = {
+    'use strict'
+  
+    module.exports = {
 
 		dataTypes: function (param) {
-			
-		}
+            if ((param === null) || (typeof param === 'undefined')) {
+                return "no value";
+            } else if (typeof param === 'number') {
+                if (param < 100) {
+                    return "less than 100";
+                } else if (param > 100) {
+                    return "more than 100";
+                } else {
+                    return "equal to 100";
+                }
+            } else if (typeof param === 'string') {
+                return param.length;
+            } else if (typeof param === 'object') {
+                return param[2];
+            } else if (typeof param === 'function') {
+                return testFunction(true);
+            } else if (typeof param === 'boolean' ) {
+                return param;
+            } else {
+				return "some other value";
+			}
+		},
 
+	}
+
+	function testFunction(arg) {
+		if(arg === true) {
+            return 'called callback';
+        }
 	}
