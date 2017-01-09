ca26ab79a10c68d854d1615da3dc4c3acb10661d
seunkoko <seunowonikoko@gmail.com>
2017-01-09 06:40:22 +0100

    updated with 4 tests

--- get-primes/tests/test.js
+++ get-primes/tests/test.js
@@ -4,6 +4,24 @@
     var getPrimes = require('../library.js').getPrimes;
 
     describe("Get Prime Tests ", function() {
-        
+        describe("Case for some falsy values", function() {
+
+            it("should return 'Invalid input' for null", function() {
+            expect(getPrimes(null)).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for undefined", function() {
+            expect(getPrimes(undefined)).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for string e.g 'test'", function() {
+            expect(getPrimes('test')).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for negative values e.g -10", function() {
+            expect(getPrimes(-10)).toEqual('Invalid input');
+            });
+
+        });
 
     });
