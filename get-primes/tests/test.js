e1a9dfa68b9aa71be61bcc72ff384237121aa7dc
seunkoko <seunowonikoko@gmail.com>
2017-01-09 06:59:34 +0100

    tests updated to 10

--- get-primes/tests/test.js
+++ get-primes/tests/test.js
@@ -14,12 +14,44 @@
             expect(getPrimes(undefined)).toEqual('Invalid input');
             });
 
-            it("should return 'Invalid input' for string e.g 'test'", function() {
+            it("should return 'Invalid input' for data types other than number e.g 'test'", function() {
             expect(getPrimes('test')).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for data types other than number e.g [3]", function() {
+            expect(getPrimes([3])).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for data types other than number e.g true", function() {
+            expect(getPrimes(true)).toEqual('Invalid input');
             });
 
             it("should return 'Invalid input' for negative values e.g -10", function() {
             expect(getPrimes(-10)).toEqual('Invalid input');
+            });
+
+        });
+
+        describe("Case for integers", function() {
+
+            it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ] for 34", function() {
+            expect(getPrimes(34)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 ]);
+            });
+
+            it("should return [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ] for 34", function() {
+            expect(getPrimes(50)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
+            });
+
+        });
+
+         describe("Case for integers 0 and 1", function() {
+
+            it("should return 'Invalid input' for 0", function() {
+            expect(getPrimes(0)).toEqual('Invalid input');
+            });
+
+            it("should return 'Invalid input' for 1", function() {
+            expect(getPrimes(1)).toEqual('Invalid input');
             });
 
         });
